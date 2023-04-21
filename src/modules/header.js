1551d553fadce6b8d3161e8bf986bed6fb397ef1
otegaa <ekiugbotega@gmail.com>
2023-04-21 11:55:06 +0200

    Update CSS styles

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,10 +1,10 @@
-// import logo from '../assets/logo.png';
+import logo from '../assets/logo.png';
 
 const header = () => {
   const nav = document.querySelector('.desktop-nav');
   nav.innerHTML = `
   <ul class="menu">
-    <a href="index.html"><img class="logo-desk" src="" alt="logo"></a> 
+    <a href="index.html"><img class="logo-desk" src="${logo}" alt="logo"></a> 
     <li><a href="#">Home</a></li>
     <li><a href="#">About</a></li>
     <li><a href="#">Movies(<span id = "counter"></span>)</a></li>
